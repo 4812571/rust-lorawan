6ffaf76e3d5ec07c46ac84f1452526c110e6d600
Dylan <57853417+4812571@users.noreply.github.com>
2023-05-02 03:37:06 -0400

    Update parse.rs

--- encoding/fuzz/fuzz_targets/parse.rs
+++ encoding/fuzz/fuzz_targets/parse.rs
@@ -1,9 +1,25 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use lorawan::parser::parse;
+use lorawan::parser::{parse, PhyPayload};
 
 fuzz_target!(|data: &[u8]| {
     let owned_copy = data.to_vec();
-    let _ = parse(owned_copy);
+    
+    let parsed = match parse(owned_copy) {
+        Ok(parsed) => parsed,
+        Err(_) => return,
+    }
+
+    match parsed {
+        JoinRequest(join_request_payload) => {
+
+        },
+        JoinAccept(join_accept_payload) => {
+
+        },
+        Data(data_payload) => {
+
+        },
+    }
 });
